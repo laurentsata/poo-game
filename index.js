889a7bd65264a3778fd0a70665061720660d478a
john Doe <sataward666@gmail.com>
2022-05-30 08:50:37 +0200

    step3 +

--- index.js
+++ index.js
@@ -48,7 +48,22 @@ projectile.draw();
 const projectiles = [];
 
 window.addEventListener("click", (event) => {
-    const projectile = new Projectile(event.clientX, event.clientY, 5, "white", null);
-    projectile.draw();
+    const angle = Math.atan2(
+        event.clientY - player.y,
+        event.clientX - player.x
+    );
+    const velocity = {
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5,
+    };
+
+    const projectile = new Projectile(
+        player.x,
+        player.y,
+        5,
+        "white",
+        velocity
+    );
     projectiles.push(projectile);
+    projectile.draw();
 });
