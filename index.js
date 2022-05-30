6d89dfadd53607bf6a0b5b21312dea967955004a
john Doe <sataward666@gmail.com>
2022-05-30 10:23:53 +0200

    resolve bugs

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-ctx.beginPath();
-ctx.arc(canvas.width / 2, canvas.height / 2, 30, 0, Math.PI * 2, false);
-ctx.fillStyle = "red";
-ctx.fill();
 
 class Entity {
     constructor(x, y, radius) {
@@ -77,17 +73,32 @@ class Projectile extends Player {
 
 // function animate
 
+class Enemy extends Projectile {
+    constructor(x, y, radius, color, velocity) {
+        super(x, y, radius, color, velocity);
+    }
+}
+
+const player = new Player(canvas.width / 2, canvas.height / 2, 10, "blue");
+
+const projectiles = [];
+
+const enemies = [];
+
 function animate() {
     requestAnimationFrame(animate);
 
-    ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     player.draw();
 
     projectiles.forEach((projectile) => projectile.update());
+
+    enemies.forEach((enemy) => enemy.update());
 }
 animate();
+
 
 //function spawnEnemies
 
