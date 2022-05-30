1e4e15b32e33488b590d88377279257788c36370
john Doe <sataward666@gmail.com>
2022-05-30 08:17:15 +0200

    step 3

---
+++ index.js
@@ -0,0 +1,54 @@
+const canvas = document.querySelector("#game-container");
+const ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+ctx.beginPath();
+ctx.arc(canvas.width / 2, canvas.height / 2, 30, 0, Math.PI * 2, false);
+ctx.fillStyle = "red";
+ctx.fill();
+
+class Entity {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = "red";
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+class Player extends Entity{
+    constructor(x, y, radius, color) {
+        super(x, y, radius);
+        this.color = color;
+    }
+}
+
+const player = new Player(canvas.width / 2, canvas.height / 2, 10, "red");
+player.draw();
+
+class Projectile extends Player {
+    constructor(x, y, radius, color, velocity) {
+        super(x, y, radius, color);
+        this.velocity = velocity;
+    }
+}
+
+const projectile = new Projectile(50, 50, 30, "blue", {x: 3, y: 3});
+projectile.draw();
+//étape 3
+const projectiles = [];
+
+window.addEventListener("click", (event) => {
+    const projectile = new Projectile(event.clientX, event.clientY, 5, "white", null);
+    projectile.draw();
+    projectiles.push(projectile);
+});
