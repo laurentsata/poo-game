7593011e5eabb023a73e2af8f5d079c575c6ac80
john Doe <sataward666@gmail.com>
2022-05-30 10:07:37 +0200

    add const projectiles

--- index.js
+++ index.js
@@ -35,6 +35,30 @@ class Player extends Entity{
 const player = new Player(canvas.width / 2, canvas.height / 2, 10, "red");
 player.draw();
 
+//const projectiles
+const projectiles = [];
+
+window.addEventListener("click", (event) => {
+    const angle = Math.atan2(
+        event.clientY - player.y,
+        event.clientX - player.x
+    );
+    const velocity = {
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5,
+    };
+
+    const projectile = new Projectile(
+        player.x,
+        player.y,
+        5,
+        "white",
+        velocity
+    );
+    projectiles.push(projectile);
+    projectile.draw();
+});
+
 //function spawnEnemies
 
 function spawnEnemies() {
